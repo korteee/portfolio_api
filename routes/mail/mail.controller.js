61e778325ec0771e35b8310cf99ad3f879309fed
APPRECOT3\Apprecot <korte.stavros@hotmail.com>
2017-10-20 15:09:51 +0300

    udpate gitignore

--- routes/mail/mail.controller.js
+++ routes/mail/mail.controller.js
@@ -1,9 +1,11 @@
 const nodemailer = require('nodemailer');
-const emailCredentials =  require('./../../email.credentials.json');
+const emailCredentials = require('./../../common/credentials.constant').email;
 
 
 const transporter = nodemailer.createTransport({
-    service: "Gmail",
+    host: "smtp.gmail.com",
+    port: 465,
+    secure: true,
     auth: {
         user: emailCredentials.username,
         pass: emailCredentials.password
@@ -23,7 +25,7 @@ function sendMail(nameFrom, emailFrom, messageFrom, mailTo, successCb, errorCb) 
     const mail = {
         from: `"${nameFrom}" <${emailFrom}>`,
         to: Array.isArray(mailTo) ? mailTo.join(', ') : mailTo,
-        subject: `Message from Official Korte's website`,
+        subject: `Message from Korte's Official  Website`,
         text: messageFrom,
     };
 
